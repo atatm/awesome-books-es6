df04a3f08bb969f55337790a9adf1bad6d86f8de
atatm <alelignayana@gmail.com>
2022-11-09 06:24:12 -0800

    Fix linter error and Add awesome book with es6 using modules

--- modules/addForm.js
+++ modules/addForm.js
@@ -1,11 +1,11 @@
-const formTitle =document.querySelector('#title');
-const formAuthor =document.querySelector('#author');
+const formTitle = document.querySelector('#title');
+const formAuthor = document.querySelector('#author');
 
 const addForm = (awesomeBookShelf) => {
-    const title = formTitle.value;
-    const author = formAuthor.value;
-    awesomeBookShelf.addBook(title,author);
-    awesomeBookShelf.saveDataToLocalStorage();
-}
+  const title = formTitle.value;
+  const author = formAuthor.value;
+  awesomeBookShelf.addBook(title, author);
+  awesomeBookShelf.saveDataToLocalStorage();
+};
 
 export default addForm;
--- modules/showMenu.js
+++ modules/showMenu.js
@@ -3,21 +3,21 @@ const form = document.querySelector('.form');
 const contact = document.querySelector('.contact');
 
 const showList = () => {
-    books.classList.remove('hide');
-    form.classList.add('hide');
-    contact.classList.add('hide');  
+  books.classList.remove('hide');
+  form.classList.add('hide');
+  contact.classList.add('hide');
 };
 
 const showAdd = () => {
-    books.classList.add('hide');
-    form.classList.remove('hide');
-    contact.classList.add('hide');  
+  books.classList.add('hide');
+  form.classList.remove('hide');
+  contact.classList.add('hide');
 };
 
 const showContact = () => {
-    books.classList.add('hide');
-    form.classList.add('hide');
-    contact.classList.remove('hide');  
+  books.classList.add('hide');
+  form.classList.add('hide');
+  contact.classList.remove('hide');
 };
 
 export { showList, showAdd, showContact };
