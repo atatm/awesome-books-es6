c5af30bedb817fc23dcf8f688fd2d9b38d92c466
atatm <alelignayana@gmail.com>
2022-11-08 19:52:29 -0800

    Use es6 modules for the Awesome book

--- index.js
+++ index.js
@@ -1,111 +1,40 @@
-const storageKey = 'awesomeBooks';
-const currentFormTitle = document.querySelector('#title');
-const currentFormAuthor = document.querySelector('#author');
-const currentFormBookAddButton = document.querySelector('#book-add-button');
+import BookShelf from './modules/bookShelf.js';
+import { showList, showAdd, showContact } from './modules/showMenu.js';
+import addForm from './modules/addForm.js';
+import { DateTime } from './modules/luxon.js';
+
 const linkAdd = document.querySelector('#link-add');
 const linkList = document.querySelector('#link-list');
 const linkContact = document.querySelector('#link-contact');
-const books = document.querySelector('.book-shelf');
-const form = document.querySelector('.form');
-const contact = document.querySelector('.contact');
+const bookAddButton = document.querySelector('#book-add-button');
 const dateTime = document.querySelector('.date-time');
-const timeNow = new Date();
-
-class BookShelf {
-  constructor() {
-    this.arrBooks = [];
-  }
-
-  addBook(newTitle, newAuthor) {
-    const newBook = {
-      id: timeNow.getTime(),
-      title: newTitle,
-      author: newAuthor,
-    };
-
-    this.arrBooks.push(newBook);
-  }
-
-  removeBook(id) {
-    this.arrBooks = this.arrBooks.filter((book) => book.id !== id);
-  }
-
-  saveDataToLocalStorage() {
-    localStorage.setItem(storageKey, JSON.stringify(this.arrBooks));
-  }
-
-  showBooks() {
-    function addElement(elementType, parent, className) {
-      const element = document.createElement(elementType);
-      element.classList.add(className);
-      parent.appendChild(element);
-      return element;
-    }
-
-    const bookList = document.querySelector('.book-list');
-    this.arrBooks.forEach((book) => {
-      const bookItem = addElement('div', bookList, 'book-item');
-      const bookTitle = addElement('div', bookItem, 'book-title');
-      bookTitle.innerHTML = `" ${book.title} "  +  by ${book.author}`;
-
-      const bookRemoveButton = addElement(
-        'button',
-        bookItem,
-        'book-remove-button',
-      );
-      bookRemoveButton.innerHTML = 'Remove';
-
-      bookRemoveButton.addEventListener('click', () => {
-        this.removeBook(book.id);
-        this.saveDataToLocalStorage();
-        window.location.reload();
-      });
-    });
-  }
-
-  loadDataFromLocalStorage() {
-    const dataLoaded = JSON.parse(localStorage.getItem(storageKey));
-    if (dataLoaded == null) {
-      this.arrBooks = [];
-    } else {
-      this.arrBooks = dataLoaded;
-    }
-
-    this.showBooks();
-  }
-}
 
 const awesomeBookShelf = new BookShelf();
 
-currentFormBookAddButton.addEventListener('click', () => {
-  const title = currentFormTitle.value;
-  const author = currentFormAuthor.value;
-  awesomeBookShelf.addBook(title, author);
-  awesomeBookShelf.saveDataToLocalStorage();
-});
-
 linkList.addEventListener('click', () => {
-  books.classList.remove('hide');
-  form.classList.add('hide');
-  contact.classList.add('hide');
+ showList();
 });
 
 linkAdd.addEventListener('click', () => {
-  books.classList.add('hide');
-  form.classList.remove('hide');
-  contact.classList.add('hide');
+  showAdd();
 });
 
 linkContact.addEventListener('click', () => {
-  books.classList.add('hide');
-  form.classList.add('hide');
-  contact.classList.remove('hide');
+ showContact();
 });
 
+bookAddButton.addEventListener('click' ,() => {
+ addForm(awesomeBookShelf); 
+});
+
+const timer = () => {
+  const now =DateTime.now ();
+  dateTime.innerHTML =now.toLocaleString(DateTime.DATETIME_MED);
+};
+
+setInterval(timer , 1000);
+
 window.onload = () => {
-  books.classList.remove('hide');
-  form.classList.add('hide');
-  contact.classList.add('hide');
   awesomeBookShelf.loadDataFromLocalStorage();
-  dateTime.innerHTML = timeNow;
+  showList();
 };
